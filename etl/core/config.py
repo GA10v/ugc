5d73a76a54cd1d8d1747f96584acc095af3747df
Galovach Andrey <a.galovach@yandex.ru>
2022-11-29 16:41:57 +0300

    task_05 Исправил config

--- etl/core/config.py
+++ etl/core/config.py
@@ -10,19 +10,34 @@ class BaseConfig(BaseSettings):
 
 
 class ClickhouseSettings(BaseConfig):
-    URL: str = 'http://localhost:8123/'
+    HOST: str = 'localhost'
+    MASTER_PORT: int = 9000
+    SUB_PORTS: list[int] = [9001, 9002]
     USER: str = None
     PASSWORD: str = None
     CLUSTER: str = 'company_cluster'
     DATABASE: str = 'default'
+    databases: list[str] = ['shard01', 'shard02', 'shard03']
 
     TABLES: list[str] = ['ugc_event']
     FIELDS: dict[str, str] = {
-        'user_id': 'VARCHAR(50) NOT NULL',
-        'movie_id': 'VARCHAR(50) NOT NULL',
-        'evetn': 'INTEGER NOT NULL',
-        'event_time': 'DATETIME NOT NULL',
+        'user_id': 'String',
+        'movie_id': 'String',
+        'evetn': 'UInt64',
+        'event_time': 'DateTime DEFAULT now()',
     }
+
+    @property
+    def client_conf(self):
+        return {
+            'host': self.HOST,
+            'port': self.MASTER_PORT,
+            'sub': self.SUB_PORTS,
+            'db': self.DATABASE,
+            'tables': self.TABLES,
+            'fields': self.FIELDS,
+            'cluster': self.CLUSTER,
+        }
 
     class Config:
         env_prefix = 'CLICKHOUSE_'
