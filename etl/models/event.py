19ac435b5ad7ab40f9fb59dcceafafe5a50bb58c
Galovach Andrey <a.galovach@yandex.ru>
2022-12-02 17:10:51 +0300

    task_08 исправил модели

--- etl/models/event.py
+++ etl/models/event.py
@@ -1,19 +1,25 @@
-from datetime import datetime
-from uuid import UUID
+from typing import Union
 
 from pydantic import BaseModel
 
 
-class Event(BaseModel):
-    user_id: UUID  # надо узнать что приходит из transform UUID | str
-    movie_id: UUID  # надо узнать что приходит из transform UUID | str
+class RatingEvent(BaseModel):
+    user_id: str
+    movie_id: str
     evetn: int
-    event_time: datetime  # надо узнать что приходит из transform datetime | str
+    event_time: str
 
-    @property
-    def dict(self) -> dict:
-        _dict = super().dict()
-        _dict['user_id'] = str(_dict['user_id'])  # удалить если приходит str
-        _dict['movie_id'] = str(_dict['movie_id'])  # удалить если приходит str
-        _dict['event_time'] = _dict['movie_id'].strftime('%Y-%m-%d %H:%M:%S')  # удалить если приходит str
-        return _dict
+
+class ViewsEvent(BaseModel):
+    user_id: str
+    movie_id: str
+    evetn: int
+    event_time: str
+
+
+events = Union[RatingEvent, ViewsEvent]
+
+
+class EventKafka(BaseModel):
+    event_type: str
+    event: events
