45049da27309b5ca9b6f045b347660162e39c8e8
Galovach Andrey <a.galovach@yandex.ru>
2022-12-15 01:28:11 +0300

    Task_06 Добавил ручки для  Bookmarks

---
+++ ugc/src/api/v1/bookmarks.py
@@ -0,0 +1,56 @@
+from http import HTTPStatus
+from typing import Optional
+
+from api.v1.utils import NotFoundError
+from fastapi import APIRouter, Depends, HTTPException
+from models.bookmarks import BookmarksSchema
+from services.bookmarks.client import BookmarkService, get_bookmark_service
+from utils import auth
+
+router = APIRouter()
+auth_handler = auth.AuthHandler()
+
+
+@router.post(
+    path='/add/{movie_id}/',
+    response_model=BookmarksSchema,
+    summary='Добавить закладку.',
+)
+async def add_bookmark(
+    movie_id: str,
+    bookmark_service: BookmarkService = Depends(get_bookmark_service),
+    _user: dict = Depends(auth_handler.auth_wrapper),
+) -> Optional[BookmarksSchema]:
+    return await bookmark_service.add(movie_id=movie_id, user_id=_user.get('user_id'))
+
+
+@router.delete(
+    path='/delete/{movie_id}/',
+    response_model=BookmarksSchema,
+    summary='Удалить закладку.',
+)
+async def delete_bookmark(
+    movie_id: str,
+    bookmark_service: BookmarkService = Depends(get_bookmark_service),
+    _user: dict = Depends(auth_handler.auth_wrapper),
+) -> Optional[BookmarksSchema]:
+    return await bookmark_service.delete(movie_id=movie_id, user_id=_user.get('user_id'))
+
+
+@router.get(
+    path='/get/{user_id}',
+    response_model=BookmarksSchema,
+    summary='Получить закладки.',
+)
+async def get_bookmark(
+    user_id: str,
+    bookmark_service: BookmarkService = Depends(get_bookmark_service),
+    _user: dict = Depends(auth_handler.auth_wrapper),
+) -> Optional[BookmarksSchema]:
+    if user_id == 'me':
+        user_id = _user.get('user_id')
+    try:
+        _doc = await bookmark_service.get(user_id=user_id)
+    except NotFoundError:
+        return HTTPException(detail='Bookmarks not found', status_code=HTTPStatus.NOT_FOUND)
+    return _doc
--- ugc/src/api/v1/utils.py
+++ ugc/src/api/v1/utils.py
@@ -1,6 +1,19 @@
 from enum import Enum
+from http import HTTPStatus
 
 
 class EventEnum(str, Enum):
     views = 'views'
     rating = 'rating'
+
+
+def response_model(data, message: str) -> dict:
+    return {
+        'data': data,
+        'code': HTTPStatus.OK,
+        'message': message,
+    }
+
+
+class NotFoundError(Exception):
+    ...
