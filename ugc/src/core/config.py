ff337bdfcf35a1c28584463de12d506e700889cc
Galovach Andrey <a.galovach@yandex.ru>
2022-12-17 16:27:37 +0300

    task_07 добавил collection и prefix

--- ugc/src/core/config.py
+++ ugc/src/core/config.py
@@ -19,6 +19,7 @@ class MongoSettings(BaseConfig):
     PORT: int = 27019
     DB: str = 'ugc_db'
     BOOKMARK: str = 'bookmark_collection'
+    RATING: str = 'rating_collection'
 
     @property
     def uri(self):
@@ -68,6 +69,7 @@ class FastapiSettings(BaseConfig):
     PORT: int = 8000
     EVENT_PREFIX: str = '/ugc_api/v1/event'
     BOOKMARK_PREFIX: str = '/ugc_api/v1/bookmark'
+    RATING_PREFIX: str = '/ugc_api/v1/rating'
 
     class Config:
         env_prefix = 'FASTAPI_'
